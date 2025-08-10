9f3bc9edb053de74c4bbea699868785af332ffa2
cn-gourav <mrgusain558@gmail.com>
2025-08-10 23:32:36 +0530

    Add :: Starter Files

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Sidebar.css";
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <div>
+                
+            </div>
+
+        </div>
+    )
+};
+
+export default Sidebar;
